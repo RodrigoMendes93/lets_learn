c62ccbd51102f1d6b9bfb8bfb12ff87d186d8788
RodrigoMendes93 <born.in.the.90s@live.com>
2023-07-05 21:40:49 -0300

    sim

--- lib/lista.dart
+++ lib/lista.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/sobre.dart';
 
 class Pagina1 extends StatefulWidget {
-  const Pagina1({super.key, required String title}): _title = title;
+  const Pagina1({super.key, required String title}) : _title = title;
 
   final String _title;
   get name => _title;
@@ -19,33 +19,36 @@ class _Pagina1State extends State<Pagina1> {
         title: Text(widget._title),
         actions: <Widget>[
           TextButton(
-            style: TextButton.styleFrom(foregroundColor: Colors.white,),
-            onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (_) => (const SobrePage())));
-            }, 
-            child: const Text('Sobre'),
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
             ),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (_) => (const SobrePage())));
+            },
+            child: const Text('Sobre'),
+          ),
         ],
       ),
       body: ListView.builder(
-          itemCount: 10,
-          physics: const AlwaysScrollableScrollPhysics(),
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: SizedBox.fromSize(
-                        size: const Size.fromRadius(20),
-                        child: Image.asset(
-                        '/images/logo.png',
-                        alignment: Alignment.centerLeft,
-                        width: 100,
-                        ),
-                      ),
-              title: Text('Atividade ${index + 1}'),
-              subtitle: Text('Atividade para estudantes do nível ${index + 1}'),
-              dense: false,
-            );
-          },
-        ),
+        itemCount: 10,
+        physics: const AlwaysScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: SizedBox.fromSize(
+              size: const Size.fromRadius(20),
+              child: Image.asset(
+                '/images/logo.png',
+                alignment: Alignment.centerLeft,
+                width: 100,
+              ),
+            ),
+            title: Text('Atividade ${index + 1}'),
+            subtitle: Text('Atividade para estudantes do nível ${index + 1}'),
+            dense: false,
+          );
+        },
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/atv1.dart';
 import 'package:flutter_application_1/lista.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: AtvPage(),
     );
   }
 }
